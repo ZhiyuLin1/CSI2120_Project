50224668c7dc8a55c1eae2cecda77464a5711b16
ZhiyuLin1 <zlin080@uottawa.ca>
2024-02-01 18:55:58 -0500

    Initial commit

---
+++ SimilaritySearch.java
@@ -0,0 +1,16 @@
+public class SimilaritySearch{
+    public static void main(String[] args){
+        String image1 = args[0];
+        String image2 = args[1];
+
+    }
+    
+}
+
+public class ColorImage{
+
+    public ColorImage(String filename){
+
+        
+    }
+}
